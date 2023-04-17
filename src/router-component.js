cce1e9f426378ab1c6251164b67aabbb28350782
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-04-17 23:24:19 +0000

    tailwind carga!

--- src/router-component.js
+++ src/router-component.js
@@ -2,6 +2,7 @@ import { LitElement, html } from "lit";
 import "./components/my-component1.js";
 import "./components/my-component2.js";
 import "./components/card.js";
+import "./components/nav.js";
 
 class RouterComponent extends LitElement {
   static properties() {
@@ -17,7 +18,8 @@ class RouterComponent extends LitElement {
       case "/":
         return html` <my-component1></my-component1>`;
       case "/contact":
-        return html` <card-component></card-component>`;
+        return html` <nav-bar></nav-bar>
+          <card-component></card-component>`;
     }
   }
 }
--- twind.config.js
+++ twind.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "@twind/core";
+import presetAutoprefix from "@twind/preset-autoprefix";
+import presetTailwind from "@twind/preset-tailwind";
 
 export default defineConfig({
-  /* @twind/with-web-components will use
-   * hashed class names in production by default
-   * If you don't want this, uncomment the next line
-   */
-  //hash: false,
+  presets: [presetAutoprefix(), presetTailwind()],
+  /* config */
 });
