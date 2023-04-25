80b5d329537d148298c9899c0bc8d7bd794da2f4
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-04-25 17:06:16 -0400

    Token header bad

--- src/controllers/getJWTHeaders.js
+++ src/controllers/getJWTHeaders.js
@@ -3,9 +3,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import express from "express";
+/* import express from "express";
 const app = express();
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./public"))); */
 
 export const getJWTHeaders = async (req, res) => {
   const token = req.query.token;
